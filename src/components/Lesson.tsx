75cfd773cef70cbb79ae77a2d461ee69641250c2
arielramosdebraga <>
2022-06-26 02:07:24 -0300

    Aula 5 • Deploy, CodeGen e próximos passos

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -22,48 +22,56 @@ export function Lesson(props: LessonProps) {
   )
 
   const isActiveLesson = slug === props.slug;
+  const availableUrlLesson = isLessonAvailable ? `/event/lesson/${props.slug}` : '';
 
   return (
-    <Link to={`/event/lesson/${props.slug}`} className="group">
-      <span className="text-gray-300">
-        {availableDateFormatted}
-      </span>
-
-      <div 
-        className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
-          'bg-green-500': isActiveLesson,
+    <>
+      <Link 
+        to={availableUrlLesson} 
+        className={classNames('group', {
+          'cursor-not-allowed': availableUrlLesson == ''
         })}
       >
-        <header className="flex items-center justify-between">
-          {isLessonAvailable ? (
-            <span className={classNames('text-sm font-medium flex items-center gap-2', {
-              'text-white': isActiveLesson,
-              'text-blue-500': !isActiveLesson
+        <span className="text-gray-300">
+          {availableDateFormatted}
+        </span>
+
+        <div 
+          className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
+            'bg-green-500': isActiveLesson,
+          })}
+        >
+          <header className="flex items-center justify-between">
+            {isLessonAvailable ? (
+              <span className={classNames('text-sm font-medium flex items-center gap-2', {
+                'text-white': isActiveLesson,
+                'text-blue-500': !isActiveLesson
+              })}>
+                <CheckCircle size={20} />
+                Conteúdo liberado
+              </span>
+            ) : (
+              <span className='text-sm text-orange-500 font-medium flex items-center gap-2'>
+                <Lock size={20} />
+                Em breve
+              </span>
+            )}
+            <span className={classNames('text-xs rounded py-[0.125rem] px-2 text-white border font-bold', {
+              'border-white': isActiveLesson,
+              'border-green-300': !isActiveLesson
             })}>
-              <CheckCircle size={20} />
-              Conteúdo liberado
+              {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
             </span>
-          ) : (
-            <span className='text-sm text-orange-500 font-medium flex items-center gap-2'>
-              <Lock size={20} />
-              Em breve
-            </span>
-          )}
-          <span className={classNames('text-xs rounded py-[0.125rem] px-2 text-white border font-bold', {
-            'border-white': isActiveLesson,
-            'border-green-300': !isActiveLesson
+          </header>
+          
+          <strong className={classNames('mt-5 block', {
+            'text-white': isActiveLesson,
+            'text-gray-200': !isActiveLesson,
           })}>
-            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
-          </span>
-        </header>
-        
-        <strong className={classNames('mt-5 block', {
-          'text-white': isActiveLesson,
-          'text-gray-200': !isActiveLesson,
-        })}>
-          {props.title}
-        </strong>
-      </div>
-    </Link>
+            {props.title}
+          </strong>
+        </div>
+      </Link>
+    </>
   )
 }
